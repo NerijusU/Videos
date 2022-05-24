334b2cd020c646bac366645cbb33f50fc2cc4a8c
Nerijus Urbonas <nerijus.urbonas@gmail.com>
2022-05-24 10:50:45 +0200

    add items list

---
+++ src/components/VideoList.js
@@ -0,0 +1,16 @@
+import VideoItem from "./VideoItem"
+
+
+const VideoList = ({videos}) => {
+    const renderedList = videos.map((video) => {
+        return <VideoItem video={video} />
+    })
+
+  return (
+    <div className="ui relaxed divided list">
+      {renderedList}
+    </div>
+  )
+}
+
+export default VideoList
