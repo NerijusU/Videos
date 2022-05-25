046b428389a923787a03924695d6b94599f9456f
Nerijus Urbonas <nerijus.urbonas@gmail.com>
2022-05-25 11:10:18 +0200

    display video player

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react'
-import SearchBar from './SearchBar'
-import youtube from "../apis/youtube"
-import VideoList from './VideoList'
-
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends Component {
-  state = { videos: [] }
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
-        q: term
-      }
-    })
+        q: term,
+      },
+    });
 
-    console.log(response.data.items)
-    this.setState({videos: response.data.items})
-  }
+    console.log(response.data.items);
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo: video})
+  };
 
   render() {
     return (
-      <div className='ui container'>
+      <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
